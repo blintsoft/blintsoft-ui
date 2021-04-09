38a67691d310499bc102f0c4811ee01f574f2cbb
Juliano Ramos <490meister@gmail.com>
2021-04-09 19:51:04 -0300

    chore(release): Publish 1.0.1-next.4

--- packages/web/lib/Input/index.tsx
+++ packages/web/lib/Input/index.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import React from 'react'
 
 // import { Container } from './styles';
 
 const Input: React.FC = () => {
-  return <div>Input</div>;
+  return <div>Input tyeste</div>
 }
 
-export default Input;
+export default Input
