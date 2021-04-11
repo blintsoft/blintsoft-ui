7ac4b0feac98616ed93cfc6e090c548eadfcd71f
Juliano Ramos <490meister@gmail.com>
2021-04-11 13:24:27 -0300

    chore(release): Publish 1.0.6

--- packages/core/lib/ThemeProvider.tsx
+++ packages/core/lib/ThemeProvider.tsx
@@ -3,11 +3,11 @@ import React from 'react'
 import { ThemeContext } from './Context'
 import { IThemeProps } from './types'
 
-const ThemeProvider: React.FC<IThemeProps> = ({ theme = {}, children }) => {
+const Theme: React.FC<IThemeProps> = ({ theme = {}, children }) => {
   console.log(theme)
   return (
     <ThemeContext.Provider value={{ theme }}>{children}</ThemeContext.Provider>
   )
 }
 
-export default ThemeProvider
+export const ThemeProvider = Theme
