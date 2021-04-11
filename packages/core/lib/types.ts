beee12b9b6ab9490cdcc9dc8a405f2d73703b39d
Juliano Ramos <490meister@gmail.com>
2021-04-11 14:36:11 -0300

    chore(release): Publish 1.0.7

--- packages/core/lib/types.ts
+++ packages/core/lib/types.ts
@@ -1,6 +1,49 @@
 import { ReactNode } from 'react'
 
-export interface ITheme {}
+export interface IPaletteBackground {
+  default: string
+  paper: string
+}
+
+export interface IPaletteText {
+  primary: string
+  secondary: string
+  disabled: string
+}
+
+export interface IPaletteColor {
+  light?: string
+  main: string
+  dark?: string
+}
+
+export interface IPalette {
+  type: 'dark' | 'light'
+  primary: IPaletteColor
+  secondary: IPaletteColor
+  error: IPaletteColor
+  warning: IPaletteColor
+  info: IPaletteColor
+  success: IPaletteColor
+  text: IPaletteText
+  background: IPaletteBackground
+  divider: string
+}
+
+export interface ISpacing {
+  sm: string
+  md: string
+  lg: string
+  xl: string
+}
+
+export interface ITheme {
+  screens: {}
+  palette: IPalette
+  fontFamily: {}
+  spacing: ISpacing
+  borderRadius: {}
+}
 
 export interface IThemeContext {
   theme: ITheme
